57dca75df0eca13916cd30b3266589b2a3ced7ee
Matvey L <matveylavrinovich@gmail.com>
2023-07-08 20:00:23 +1200

    Config fixes

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import * as Cesium from 'cesium';
-import * as BM from 'bruce-models';
-import * as BC from 'bruce-cesium';
+import * as Cesium from "cesium";
+import * as BM from "bruce-models";
+import * as BC from "bruce-cesium";
 
 window.Cesium = Cesium;
 window.BModels = BM;
 window.BEngine = BC;
-window.CESIUM_BASE_URL = 'https://matvey-tk.github.io/nextspace-web-bundle/dist/';
+window.CESIUM_BASE_URL = "https://nextspace-limited.github.io/nextspace-web-bundle/dist/";
